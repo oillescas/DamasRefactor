fc92a386af2b29f94370551b3085041752b5c8b0
Oscar Illescas <oscar.illescas@gmail.com>
2019-11-09 16:03:14 +0100

    Eliminar Session: eliminar resto de metodos

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Session.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Session.java
@@ -11,30 +11,6 @@ public class Session {
 		this.game = game;
 	}
 
-	public Error move(Coordinate origin, Coordinate target) {
-		return this.game.move(origin, target);
-	}
-
-	public Piece getPiece(Coordinate coordinate) {
-		return this.game.getPiece(coordinate);
-	}
-
-	public Color getColor() {
-		return this.game.getColor();
-	}
-
-	public Color getColor(Coordinate coordinate) {
-		return this.game.getColor(coordinate);
-	}
-
-	public boolean isBlocked() {
-		return this.game.isBlocked();
-	}
-
-	public int getDimension() {
-		return this.game.getDimension();
-	}
-
 	
 
 }
